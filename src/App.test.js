97a14206592c8f07945603c2b6afa152e03866e1
Darciechapman <darciechapman96@gmail.com>
2020-07-15 12:50:27 +0800

    replacing files with react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { IconToggle } from "react-mdl";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-IconToggle("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
